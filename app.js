1ccdc03d09bf37da1f1176863f7c926409619a1d
a12331181 <a12331181@gmail.com>
2021-04-18 14:56:16 +0800

    feat: add edit.handlebars

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ app.get('/expense-tracker/new', (req, res) => {
   res.render('new')
 })
 
+app.get('/expense-tracker/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record }))
+    .catch(error => console.log(error))
+})
+
 app.post('/expense-tracker', (req, res) => {
   const name = req.body.name
   const date = req.body.date
@@ -40,6 +48,24 @@ app.post('/expense-tracker', (req, res) => {
   return Record.create({ name,date,category,amount })   
     .then(() => res.redirect('/')) 
     .catch(error => console.log(error))
+})
+
+app.post('/expense-tracker/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const date = req.body.date
+  const category = req.body.category
+  const amount = req.body.amount 
+  return Record.findById(id)
+  .then(record =>{
+    record.name = name
+    record.date = date
+    record.category = category
+    record.amount = amount
+    return record.save()
+  })
+  .then(()=> res.redirect('/'))
+  .catch(error => console.log(error))
 })
 
 app.listen(3000,() => {
